a0920b7f25f3c47486f08e0fd34fb48075ef9a86
deur2008 <deur2010@hotmail.com>
2018-04-25 17:56:54 +0800

    Initial commit

---
+++ app/src/main/java/tw/edu/ntut/csie/app02_103590054/MainActivity.java
@@ -0,0 +1,13 @@
+package tw.edu.ntut.csie.app02_103590054;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
